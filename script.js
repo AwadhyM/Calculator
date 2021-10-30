bb6dac31a25aa41a8702f4d2ae9ab22d9a21d659
awadhy <awadhym@outlook.com>
2021-10-30 12:57:28 +0100

    Created operate function

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ calculation = {
 }
 }
 
+const operate = (operator,previousNum, secondNum) => {
+    if (operator === 'addition') {
+        return calculation.addition(previousNum, secondNum);
+    } else if (operator === 'subtraction') {
+        return calculation.subtraction(previousNum, secondNum);
+    } else if (operator === 'division') {
+        return calculation.division(previousNum, secondNum);
+    } else if (operator === 'multiplication') {
+        return calculation.multiplication(previousNum, secondNum);
+    }
+}
+
+//let operator = 'multiplication';
+//let previousNum = 12;
+//let secondNum = 12;
+
+//console.log(operate(operator, previousNum, secondNum));
