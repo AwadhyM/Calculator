098be05c63a7db0ad616340a274b7b50a1989a74
awadhy <awadhym@outlook.com>
2021-10-30 12:47:09 +0100

    Created functions for calculator operations and stored in a project

---
+++ script.js
@@ -0,0 +1,15 @@
+calculation = {
+    addition(previousNum, secondNum) {
+        return (previousNum + secondNum);
+    },
+    subtraction(previousNum, secondNum) {
+        return (previousNum - secondNum);
+    },
+    multiplication(previousNum, secondNum) {
+        return previousNum * secondNum;
+    },
+    division(previousNum, secondNum) {
+        return previousNum / secondNum; 
+}
+}
+
