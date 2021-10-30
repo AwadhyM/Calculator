0b832421934c1540af8513b39f859ff09aa7bc64
awadhy <awadhym@outlook.com>
2021-10-30 20:20:44 +0100

    Added functionality to the calculator. It now currently carries out operations on a pair of numbers that can be stringed together.

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+const calculatorDisplay = document.getElementById("display");
+let numberButtons = document.querySelectorAll('.numericalButton');
+let operatorButtons = document.querySelectorAll('.operatorButton');
+let equalButton = document.getElementById('equal');
+let userInput1 = document.createElement('h3');
+let userInput2 = document.createElement('h3');
+userInput1.className="calculatorOutput"
+calculatorDisplay.appendChild(userInput1);
+calculatorDisplay.appendChild(userInput2);
+let previousNum;
+let secondNum;
+let newNum;
+
+let operator;
+
+
+const populateDisplay = numberButtons.forEach((button) => {
+    button.addEventListener('click', function (e) {
+        userInput1.textContent += `${e.target.id}`;
+    if (e.target.id === '+' || e.target.id === '-' || e.target.id === '/' || e.target.id === 'x') {
+        operator = e.target.id;
+        previousNum = userInput1.textContent;
+        previousNum = parseFloat(previousNum);
+        userInput1.textContent = '';
+    }
+    if (e.target.id === '=' && (operator === '+' || operator === '-' || operator === 'x' || operator === '/')) {
+        secondNum = userInput1.textContent;
+        secondNum = parseFloat(secondNum);
+        userInput1.textContent = operate(operator, previousNum, secondNum);
+    }
+    })
+});
+
 calculation = {
     addition(previousNum, secondNum) {
         return (previousNum + secondNum);
@@ -14,19 +47,16 @@ calculation = {
 }
 
 const operate = (operator,previousNum, secondNum) => {
-    if (operator === 'addition') {
+    if (operator === '+') {
         return calculation.addition(previousNum, secondNum);
-    } else if (operator === 'subtraction') {
+    } else if (operator === '-') {
         return calculation.subtraction(previousNum, secondNum);
-    } else if (operator === 'division') {
+    } else if (operator === '/') {
         return calculation.division(previousNum, secondNum);
-    } else if (operator === 'multiplication') {
+    } else if (operator === 'x') {
         return calculation.multiplication(previousNum, secondNum);
+    } else {
+        return 'ERROR';
     }
 }
 
-//let operator = 'multiplication';
-//let previousNum = 12;
-//let secondNum = 12;
-
-//console.log(operate(operator, previousNum, secondNum));
