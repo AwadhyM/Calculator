2ea113a34016e10a27fba29a5a3deeeca7266e60
awadhy <awadhym@outlook.com>
2021-10-31 20:57:08 +0000

    refactored code. Breaking main function into smaller ones. Added code that now prevents any errors from the user pressing an operator button multiple times

--- script.js
+++ script.js
@@ -2,24 +2,29 @@ const calculatorDisplay = document.getElementById('display');
 const clearButton = document.getElementById('AC');
 let deleteButton = document.getElementById('del');
 let numberButtons = document.querySelectorAll('.numericalButton');
+let operationButtons = document.querySelectorAll('.operationButton');
+let operateButton = document.getElementById('=');
 let userInput1 = document.createElement('h3');
 calculatorDisplay.appendChild(userInput1).className="calculatorOutput";
 
 
 let previousNum;
 let secondNum;
-let operator;
+let operator = 'default'; 
+
+
+
 
 calculator = {
     operate(operator,previousNum, secondNum) {
         if (operator === '+') {
-            return calculation.addition(previousNum, secondNum);
+            return userInput1.textContent = calculation.addition(previousNum, secondNum);
         } else if (operator === '-') {
-            return calculation.subtraction(previousNum, secondNum);
+            return userInput1.textContent = calculation.subtraction(previousNum, secondNum);
         } else if (operator === '/') {
-            return calculation.division(previousNum, secondNum);
+            return userInput1.textContent = calculation.division(previousNum, secondNum);
         } else if (operator === 'x') {
-            return calculation.multiplication(previousNum, secondNum);
+            return userInput1.textContent = calculation.multiplication(previousNum, secondNum);
         } else {
             return 'ERROR';
         }
@@ -28,6 +33,7 @@ calculator = {
         userInput1.textContent = ''; 
         previousNum = 0;
         secondNum = 0;
+        operator = 'default';
     },
     del() {
         userInput1.textContent = userInput1.textContent.slice(0,-1);
@@ -38,26 +44,34 @@ deleteButton.addEventListener('click', calculator.del);
 clearButton.addEventListener('click', calculator.clear);
 
 const populateDisplay = numberButtons.forEach((button) => {
-    button.addEventListener('click', function (e) {
-       let initialInput = userInput1.textContent += `${e.target.id}`;
-       if (initialInput === '=') {
-           userInput1.textContent = ''; 
-       }
-       if (e.target.id === '+' || e.target.id === '-' || e.target.id === '/' || e.target.id === 'x') {
-        operator = e.target.id;
-        previousNum = userInput1.textContent;
-        previousNum = previousNum.replace(/[^a-zA-Z0-9]/g,'');
-        previousNum = parseFloat(previousNum);
-        userInput1.textContent = '';
-       }
-       if (e.target.id === '=' && (operator === '+' || operator === '-' || operator === 'x' || operator === '/')) {
-        secondNum = userInput1.textContent;
-        secondNum = secondNum.replace(/[^a-zA-Z0-9]/g,'');
-        secondNum = parseFloat(secondNum);
-        userInput1.textContent = calculator.operate(operator, previousNum, secondNum);
-       }
+    button.addEventListener('click', function(e) {
+        let initialInput = userInput1.textContent += `${e.target.id}`;
     })
-});
+})
+
+const operationButton = operationButtons.forEach((button) => {
+    button.addEventListener('click', function(e) {
+    if (operator != 'default') {
+        return;
+    } else {
+        previousNum = userInput1.textContent;
+        userInput1.textContent = '';
+    }
+    operator = e.target.id;
+})
+})
+
+
+const Equal = () => {
+     previousNum = parseFloat(previousNum);
+     secondNum = userInput1.textContent;
+     secondNum = parseFloat(secondNum);
+     console.log(calculator.operate(operator, previousNum, secondNum));
+     operator = 'default';
+}
+
+operateButton.addEventListener('click', Equal);
+
 
 calculation = {
     addition(previousNum, secondNum) {
@@ -77,4 +91,5 @@ calculation = {
         } 
 }
 }
+
 
