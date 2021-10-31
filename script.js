8b449f56a1dda7d98906c4afb1e211ed781d0326
awadhy <awadhym@outlook.com>
2021-10-31 21:26:17 +0000

    Refactored code

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 const calculatorDisplay = document.getElementById('display');
 const clearButton = document.getElementById('AC');
-let deleteButton = document.getElementById('del');
-let numberButtons = document.querySelectorAll('.numericalButton');
-let operationButtons = document.querySelectorAll('.operationButton');
-let operateButton = document.getElementById('=');
-let userInput1 = document.createElement('h3');
+const deleteButton = document.getElementById('del');
+const numberButtons = document.querySelectorAll('.numericalButton');
+const operationButtons = document.querySelectorAll('.operationButton');
+const operateButton = document.getElementById('=');
+const userInput1 = document.createElement('h3');
 calculatorDisplay.appendChild(userInput1).className="calculatorOutput";
-
 
 let previousNum;
 let secondNum;
 let operator = 'default'; 
-
-
-
 
 calculator = {
     operate(operator,previousNum, secondNum) {
@@ -37,17 +33,19 @@ calculator = {
     },
     del() {
         userInput1.textContent = userInput1.textContent.slice(0,-1);
-       }
+       },
+    equal() {
+        previousNum = parseFloat(previousNum);
+        secondNum = userInput1.textContent;
+        secondNum = parseFloat(secondNum);
+        calculator.operate(operator, previousNum, secondNum);
+        operator = 'default';
+   }
 }
 
 deleteButton.addEventListener('click', calculator.del);
 clearButton.addEventListener('click', calculator.clear);
 
-const populateDisplay = numberButtons.forEach((button) => {
-    button.addEventListener('click', function(e) {
-        let initialInput = userInput1.textContent += `${e.target.id}`;
-    })
-})
 
 const operationButton = operationButtons.forEach((button) => {
     button.addEventListener('click', function(e) {
@@ -60,15 +58,6 @@ const operationButton = operationButtons.forEach((button) => {
     operator = e.target.id;
 })
 })
-
-
-const Equal = () => {
-     previousNum = parseFloat(previousNum);
-     secondNum = userInput1.textContent;
-     secondNum = parseFloat(secondNum);
-     console.log(calculator.operate(operator, previousNum, secondNum));
-     operator = 'default';
-}
 
 operateButton.addEventListener('click', Equal);
 
